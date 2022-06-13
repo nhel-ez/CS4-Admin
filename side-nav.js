3d37e4ccd75e696f720ee6c318df882bc2e235c3
nhel-ez <leonelesguerra0401@gmail.com>
2022-06-13 14:45:34 +0800

    first commit

---
+++ side-nav.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+    
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function openNav2() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main2").style.marginLeft = "325px";
+    document.getElementById("main3").style.marginLeft = "20px";
+}
+    
+function closeNav2() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main2").style.marginLeft = "20px";
+    document.getElementById("main3").style.marginLeft = "20px";
+}
